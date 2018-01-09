ebfd7396fe37b53a3e044124212c545e12710484
ygrgd <laizhu33@163.com>
2018-01-10 00:04:21 +0800

    完成版本

--- 交易系统/2_获取day和tick数据.py
+++ 交易系统/2_获取day和tick数据.py
@@ -10,60 +10,49 @@ from matplotlib.pylab import date2num
 import datetime
 import time
 import glob
-
-
 import datetime
 
-
+# 获取目录下所有的文件名称，type为list
 a = glob.glob('e:/git/python/交易系统/data-day/*.csv')
 type(a)
+
 #获取今日的日期
 today =  (time.strftime("%F"))
+print(today)
 
-#读取股票代码 myportfolio
+#读取持仓文件，存入mp中，把stock列补充为6位。
 mp = pd.read_csv('data-protfolio/myportfolio.csv',index_col =0)
 mp.columns = ['name', 'stock', 'buy_date', 'buy_price', 'buy_amount', 'buy_value',
        'now_date', 'now_price', 'now_value', 'now_profit', 'now_percent']
-mp['stock']=mp['stock'].astype(str).str.zfill(6) #填充为6位
+
+#先把stock列填充为6位，然后再改成字符串格式
+mp['stock']=mp['stock'].astype(str).str.zfill(6) 
+mp['stock'] = mp['stock'].astype('str')
+
 
 #df = pd.read_excel ("test.xlsx" , converters={‘类别编码‘:str}) 另一种处理代码方法
 ######没有找到生成多个df的方法########
 
+#存储持仓数据到源文件，使用uft-8编码
 mp.to_csv("data-protfolio/myportfolio.csv",encoding = "utf-8")
 
-#提取股票代码并转换成列表 mystock  ms = mp.stock.tolist()
-# 另一种简单方式 # ms1=mp['stock']
+#提取股票代码并转换成列表 mystock  ms = mp.stock.tolist()可以用简单方法mp['stock']
 for i in mp['stock']:
+    print()
     tp = ts.get_k_data('%s' %(i))
     tp.to_csv("e:/git/python/交易系统/data-day/%s.csv" %(i),encoding = "utf-8")
 
 # tp.to_excel("e:/git/python/交易系统/data-day/%s.xlsx" %(i))
 
-for i in mp['stock']:
-    print(i)
-    df=pd.DataFrame()
-    i = df
 
-
-
-
-'''
-for i in mp['stock']:
-    print(i)
-    if i<10000:
-        i = ('000%s' %(i))
-        print(i)
-        tp = ts.get_k_data('%s' %(i))
-        tp.to_excel("e:/git/python/交易系统/data-day/%s.xlsx" %(i))
-    else:
-        print(i)
-        tp = ts.get_k_data('%d' %(i))
-        tp.to_excel("e:/git/python/交易系统/data-day/%d.xlsx" %(i))
- '''       
-#########以下为 获取tick数据        
-# 日期处理,获取前n天的日期
+     
+#########以下为 获取tick数据    
+    
+#获取今天的日期
 date = []
 cur_date = datetime.date.today() 
+
+#把前10天的日期生成date列表
 for i in range(0,10):
     print()
     day = cur_date - datetime.timedelta(days= i)
@@ -72,27 +61,14 @@ for i in range(0,10):
     date.append(day)
     print(date)
     
+#获取tick数据并存储
 for day in date:
     for i in mp['stock'][4:]:
         print(type(i))
-        tick =ts.get_tick_data(i,date = day)
+        tick =ts.get_tick_data(i,date = day,pause=5)
         tick.to_csv("e:/git/python/交易系统/data-tick/%s-%s.csv" %(i,day),encoding = "utf-8")
     
-    
-    
-    
-for i in mp['stock'][:4]:
-    tp = ts.get_k_data('%s' %(i))
-    tp.to_csv("e:/git/python/交易系统/data-day/%s.csv" %(i),encoding = "utf-8")
   
-    
-    
-yesterday = cur_date - datetime.timedelta(days=1)
-
-
-
-df = ts.get_tick_data('000586',date='2014-01-09')
-
 
 
 
